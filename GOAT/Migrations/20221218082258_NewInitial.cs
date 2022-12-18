5860944593359910c765a606cfdb480354a59bc5
nalee97 <mohamednaleef1564@gmail.com>
2022-12-18 13:55:18 +0530

    database update

---
+++ GOAT/Migrations/20221218082258_NewInitial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GOAT.Migrations
+{
+    public partial class NewInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
